c3c63877016bad00af2d4dd93c90a9f5e3c852a0
Miles Lucas <mdlucas@hawaii.edu>
2019-10-13 12:54:20 -1000

    set up vuepress

---
+++ src/.vuepress/components/Docs.vue
@@ -0,0 +1,14 @@
+<template>
+  <span id="docs">
+    <!-- <font-awesome-icon icon="book" /> -->
+    <a :href=url>Docs</a>
+  </span>
+</template>
+
+<script>
+
+export default {
+  name: "Docs",
+  props: ["url"]
+};
+</script>
---
+++ src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: "JuliaAstro",
+    description: "Community Astronomy and Astrophysics packages for Julia",
+    themeConfig: {
+        nav: [
+            { text: "Ecosystem", link: "/#ecosystem"},
+            { text: "Other Packages", link: "/#other-packages"},
+            { text: "Contributing", link: "/#contributing"},
+            { text: "GSoC", link: "/#google-summer-of-code"}
+        ]
+    }
+}
